14c075d1bc5dbf4d65852cc6a60bc9c3ed7dd4af
Jacob Brown <jbrow4@gmail.com>
2023-02-01 15:01:31 -0500

    analytics graphs

--- src/components/AnalyticsPage.tsx
+++ src/components/AnalyticsPage.tsx
@@ -3,6 +3,8 @@ import { Chart } from "react-google-charts";
 import { useTheme } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import { categories, sections, transactions } from '../recoil/tableAtoms';
+import { useRecoilValue } from 'recoil';
 
 export const data = [
     ["Task", "Hours per Day"],
@@ -13,10 +15,38 @@ export const data = [
     ["Sleep", 7],
 ];
 
-
-
 export default function AnalyticsPage() {
+    const categoriesArray = useRecoilValue(categories)
+    const sectionsArray = useRecoilValue(sections)
     const theme = useTheme()
+
+    function sumCat(idVal: string) {
+        return categoriesArray.filter(x => x.sectionID === idVal).reduce((accumulator, object) => {
+            return accumulator + object.amount;
+        }, 0)
+    }
+
+    let incomeArray = categoriesArray.filter(x => sectionsArray.find(row => row.recordID === x.sectionID)?.sectionType === "income").map(y => (
+        [
+            y.categoryName,
+            y.amount
+        ]
+    ))
+    let expenseArray = sectionsArray.filter(x => x.sectionType === "expense").map(y => (
+        [
+            y.sectionName,
+            sumCat(y.recordID)
+        ]
+    ))
+        
+    let incomeColumns = [["Income Title", "Amount"]]
+    let expenseColumns = [["Columns Title", "Amount"]]
+    //@ts-ignore
+    let incomeGraphData = incomeColumns.concat(incomeArray)
+    //@ts-ignore
+    let expenseGraphData = expenseColumns.concat(expenseArray)
+    console.log(expenseGraphData)
+
     const options = {
         title: "My Daily Activities",
         backgroundColor: "rgba(0,0,0,0)"
@@ -27,7 +57,16 @@ export default function AnalyticsPage() {
                 <Box sx={{ width: '100%' }}>
                     <Chart
                         chartType="PieChart"
-                        data={data}
+                        data={incomeGraphData}
+                        options={options}
+                        width={"100%"}
+                        height={"400px"}
+                    />
+                </Box>
+                <Box sx={{ width: '100%' }}>
+                    <Chart
+                        chartType="PieChart"
+                        data={expenseGraphData}
                         options={options}
                         width={"100%"}
                         height={"400px"}
